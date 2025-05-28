4072ea900b15c3f4e0956ba5754d5ddc6c16e71a
PeterDayne <chavez.ruiz.pedro607@gmail.com>
2025-05-28 10:31:44 -0600

    propiedades de los componentes

--- tailwind-app/src/App.jsx
+++ tailwind-app/src/App.jsx
@@ -1,8 +1,23 @@
 import './App.css'
 import Card from './Card'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from './About';
+import ContactUs from './ContactUs';
+import TeamSection from './TeamSection'; 
 
+function App({propos}) {
 
-function App() {
+  return(
+    //...
+    <BrowserRouter>
+      <Routes>
+        <Route path="Contact" element={<ContactUs/>} />
+        <Route path="Team" element={<TeamSection/>} />
+        <Route path="About" element={<About/>} />
+      </Routes>
+    </BrowserRouter>
+    //...
+  );
   
   const products = [
     {
